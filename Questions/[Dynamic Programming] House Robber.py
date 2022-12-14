c0165c8a8885e7d267032a843a9fa5ef7aebb64d
kunal5042 <78603700+kunal5042@users.noreply.github.com>
2022-12-14 10:21:59 +0530

    Update [Dynamic Programming] House Robber.py

--- Questions/[Dynamic Programming] House Robber.py
+++ Questions/[Dynamic Programming] House Robber.py
@@ -4,17 +4,19 @@
 from typing import Optional, List
 
 class Solution:
+    # O(n) time and O(1) space
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
+        if len(nums) <= 2: return max(nums)
         
-        first, second = 0, 0
+        previous = 0
+        current  = 0
         
         for idx in range(len(nums)):
-            temp   = second
-            second = max(first, second, first + nums[idx])
-            first  = temp
-        
-        return max(first, second)
+            temp = current
+            current  = max(previous + nums[idx], current)
+            previous = temp
+            
+        return current
 '''
 
 # Kunal Wadhwa
